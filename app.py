e36c9647d8a9a971afd1ccd0e6524618c39d1e08
Priya <priyaanka2235@gmail.com>
2026-04-21 10:12:05 +0000

    updated again API

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return jsonify({"message": "Updated API "})
+    return jsonify({"message": "Updated again API "})
 
 @app.route("/user")
 def user():
